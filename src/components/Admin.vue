dab62efb2d779de9851f1ab67b62c4d27466213c
Lz12 <212225353@qq.com>
2020-05-12 17:27:30 +0800

    添加admin中的子页面

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -7,13 +7,14 @@
       后台管理
     </el-header>
     <el-container>
-      <el-aside style="height: 100%">
+      <el-aside width="200px">
         <el-menu
           default-active="2"
           background-color="#545c64"
           text-color="#fff"
           active-text-color="#ffd04b"
-          
+          class="el-menu-vertical-demo"
+          style="width: 100%;height: 100%"
         > <!--collapse 是否水平收起菜单-->
           <!--使用element-ui的菜单，在SubMenu Attribute中有一个index的属性，如果index的值从后端传入，则会报错-->
           <!-- index的值不能有空格，并且要为字符串类型，将其转为字符串就不会报错了-->
@@ -83,6 +84,10 @@
 </script>
 
 <style scoped>
+  .el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
   .el-header {
     background-color: #2b303b;
     color: #fff;
--- src/components/admin/article.vue
+++ src/components/admin/article.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <h1>文章管理</h1>
-      <div style="background-color: orange;width: 100%;height: 100%">摩羯阿婆</div>
+      <div style="background-color: orange">摩羯阿婆</div>
     </div>
 </template>
 
